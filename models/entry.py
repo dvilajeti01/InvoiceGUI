eec39758f59042f0e4bda956ee4f2ae901c7a0f4
Daniel Vilajeti <daniel01.nyx@gmail.com>
2020-11-30 23:51:25 -0500

    Added JSONEncoder & add'l member functions to Entry class

--- models/entry.py
+++ models/entry.py
@@ -1,3 +1,6 @@
+from json import JSONEncoder
+
+
 class Entry:
     def __init__(self, date, description, quantity, rate):
 
@@ -6,6 +9,35 @@ class Entry:
         self.quantity = quantity
         self.rate = rate
 
+    def to_dict(self):
+
+        return {
+            "Date": self.date,
+            "Description": self.description,
+            "Quantity": self.quantity,
+            "Rate": self.rate
+        }
+
     def to_tuple(self):
 
         return (self.date, self.description, self.quantity, self.rate)
+
+    @staticmethod
+    def from_tuple(values):
+        date, description, quantity, rate = values
+
+        return Entry(date, description, quantity, rate)
+
+    def __eq__(self, other):
+        this_values = self.to_tuple()
+        other_values = other.to_tuple()
+
+        if this_values == other_values:
+            return True
+        else:
+            return False
+
+
+class EntryEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
